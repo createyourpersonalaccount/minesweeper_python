9b8255c54e8440b6b65f7baefc67c4831b1448a0
nzevgolisda <nzevgolisda@gmail.com>
2022-02-22 12:07:45 +0200

    Created an empty pygame project.

--- src/minesweeper/minesweeper.py
+++ src/minesweeper/minesweeper.py
@@ -12,3 +12,20 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import pygame
+pygame.init()
+win = pygame.display.set_mode((500,500))
+pygame.display.set_caption("Minesweeper")
+run = True
+while run:
+    pygame.time.delay(100)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_q]:
+        pygame.quit()
+    win.fill((0,0,0))  # Fills the screen with black
+    pygame.display.update() 
+pygame.quit()
